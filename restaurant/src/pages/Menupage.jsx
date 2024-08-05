aada441a1aa48991a7affddb0152b4332213c622
mahaazkar321 <mahaazkar@gmail.com>
2024-08-05 10:01:56 +0500

    res

--- restaurant/src/pages/Menupage.jsx
+++ restaurant/src/pages/Menupage.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-const Menu = () => {
+const Menupage = ({ searchQuery }) => {
   const [menu, setMenu] = useState([]);
+  const [filteredMenu, setFilteredMenu] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/menu")
       .then((response) => response.json())
-      .then((data) => setMenu(data))
+      .then((data) => {
+        setMenu(data);
+        setFilteredMenu(data);
+      })
       .catch((error) => console.error("Error fetching menu data:", error));
   }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      const filtered = menu.filter((item) =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredMenu(filtered);
+    } else {
+      setFilteredMenu(menu);
+    }
+  }, [searchQuery, menu]);
 
   return (
     <>
@@ -18,22 +33,26 @@ const Menu = () => {
         </h1>
 
         <div className="box-container">
-          {menu.map((item, index) => (
-            <div className="box" key={index}>
-              <img src={item.img} alt={item.name} />
-              <h3>{item.name}</h3>
-              <div className="price">
-                {item.price} <span>{item.originalPrice}</span>
+          {filteredMenu.length > 0 ? (
+            filteredMenu.map((item, index) => (
+              <div className="box" key={index}>
+                <img src={item.img} alt={item.name} />
+                <h3>{item.name}</h3>
+                <div className="price">
+                  {item.price} <span>{item.originalPrice}</span>
+                </div>
+                <a href="#" className="btn">
+                  add to cart
+                </a>
               </div>
-              <a href="#" className="btn">
-                add to cart
-              </a>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No items match your search criteria.</p>
+          )}
         </div>
       </section>
     </>
   );
 };
 
-export default Menu;
+export default Menupage;
